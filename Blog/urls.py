04c3e29dcc409c039712166f2c182c86dc3dd8ca
habiboolean <habiboolean@gmail.com>
2022-06-28 13:52:17 +0300

    added EDIT link to post_details for author

--- Blog/urls.py
+++ Blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', PostList.as_view()),
     path('author/<int:author_id>/', PostListUser.as_view(), name='author_filter'),
     path('create/', PostCreate.as_view(), name='post_create'),
-    path('edit/<slug:slug>/', PostEdit.as_view()),
+    path('edit/<slug:slug>/', PostEdit.as_view(), name='post_edit'),
     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
 ]
